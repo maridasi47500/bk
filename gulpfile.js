c70fd382b293f9dc77c0b93e76bcb856bee0c521
theNonDangerousButInvisibleAndAnonymousCoder <thecat@thedog.themouse>
2022-11-06 19:17:22 -0300

    ionic

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+  const sass = require('gulp-sass')(require('sass'));
+
+var cleanCss = require('gulp-clean-css');
+var rename = require('gulp-rename');
+
+var paths = {
+  sass: ['./scss/**/*.scss']
+};
+
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function(done) {
+  gulp.src('./scss/*.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./www/css/'))
+    .pipe(cleanCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./www/css/'))
+    .on('end', done);
+});
+
+gulp.task('watch', ['sass'], function() {
+  gulp.watch(paths.sass, ['sass']);
+});
